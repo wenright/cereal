73140e6252b4d81ba35ed2c8f88e35fe434af07c
Will Enright <wenright@purdue.edu>
2016-04-08 20:29:59 -0400

    Add a few more tests

--- test.lua
+++ test.lua
@@ -1,6 +1,30 @@
 describe('Cereal', function()
   Cereal = require'cereal'
 
+  it('failed serializing a single number', function()
+    local expected = 5
+    local result = Cereal.load(Cereal.tostring(expected))
+    assert.are.same(expected, result)
+  end)
+
+  it('failed serializing a single string', function()
+    local expected = 'Hello world!'
+    local result = Cereal.load(Cereal.tostring(expected))
+    assert.are.same(expected, result)
+  end)
+
+  it('failed serializing a nil value', function()
+    local expected = nil
+    local result = Cereal.load(Cereal.tostring(expected))
+    assert.are.same(expected, result)
+  end)
+
+  it('failed serializing a simple table with nil values', function()
+    local expected = {x = nil, y, z}
+    local result = Cereal.load(Cereal.tostring(expected))
+    assert.are.same(expected, result)
+  end)
+
   it('failed serializing a simple table with numbers', function()
     local expected = {x = 5, y = 22}
     local result = Cereal.load(Cereal.tostring(expected))
